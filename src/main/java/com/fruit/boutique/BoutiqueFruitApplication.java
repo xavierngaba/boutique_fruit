9c5f14ce81963132267463fb0452643daf385dde
xavier_ng <xavierngaba@gmail.com>
2023-08-10 21:40:13 +0200

    boutique-fruit : init project

---
+++ src/main/java/com/fruit/boutique/BoutiqueFruitApplication.java
@@ -0,0 +1,13 @@
+package com.fruit.boutique;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BoutiqueFruitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BoutiqueFruitApplication.class, args);
+	}
+
+}
